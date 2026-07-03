93d3b0cb8ee69e442de39a79c0d6d06fc2549955
Leif Eriksen <leif.eriksen.au@gmail.com>
2026-07-03 17:42:04 +1000

    feat: add container_names output
    Exposes the created filesystem (container) names, derived from the resources, so consumers can take an implicit dependency on the containers existing.

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,14 @@ output "filesystem_ids" {
   description = "Map of container name to Data Lake Gen2 filesystem resource ID."
 }
 
+output "container_names" {
+  # Derived from the filesystem resources (not var.containers) so consumers get
+  # an implicit dependency on the containers actually existing, while preserving
+  # input order.
+  value       = [for c in var.containers : azurerm_storage_data_lake_gen2_filesystem.this[c.container_name].name]
+  description = "Names of the created filesystems (containers), in input order."
+}
+
 output "path_ids" {
   value       = { for k, v in azurerm_storage_data_lake_gen2_path.this : k => v.id }
   description = "Map of \"<container>::<path>\" to Data Lake Gen2 path resource ID."
